10b9ef9bd3afcf92922836db0d9ec21436acf86c
YZ318 <YakovZhuravlev88@gmail.com>
2023-05-17 16:23:58 +0300

    Задача 41

---
+++ HomeW41/Program.cs
@@ -0,0 +1,20 @@
+﻿int m, count = 0;
+
+// чтение количества чисел, которые нужно ввести
+Console.WriteLine("Введите количество чисел, которые нужно ввести: ");
+m = int.Parse(Console.ReadLine());
+
+// чтение чисел и подсчёт количества чисел больше 0
+for (int i = 0; i < m; i++)
+{
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+
+    if (number > 0)
+    {
+        count++;
+    }
+}
+
+// вывод результата на экран
+Console.WriteLine("Количество чисел, больше 0: {0}", count);
